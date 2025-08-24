3bc35caaf6e4bd0b5756c1eeb24503cafa818d75
yousuf ali <yousuf787700@gmail.com>
2025-08-24 10:35:42 +0600

    addes home page hadet

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 document.getElementById('updateBtn')
     .addEventListener('click', function(e){
          e.preventDefault()
-         const number = 1842846581;
+         const number = "01842846581";
          const pin = 6581;
          const inputNumber =document.getElementById('input_Number');
-         const inputNumberValue =parseInt(inputNumber.value);
+         const inputNumberValue =inputNumber.value;
 
         const inputPin =document.getElementById('input_pin');
         const inputPinValue = parseInt(inputPin.value );
