6b27efa3c126e635555604c5e4af12d26ea34a87
yousuf ali <yousuf787700@gmail.com>
2025-08-25 01:14:00 +0600

    added transactions functoin

--- home.js
+++ home.js
@@ -53,30 +53,120 @@
    
 
 
-  
+      // Pay Bil
+
+      document.getElementById('PayBilBtn')
+         .addEventListener('click', function(e){
+            e.preventDefault()
+             const payAmountBill= parseInt(document.getElementById('payAmount').value);
+
+             const afterPayBilBlanc =parseInt(document.getElementById('amountCount').innerText);
+             const payBilAvailableBlanc = afterPayBilBlanc - payAmountBill;
+             console.log(payBilAvailableBlanc)
+
+             document.getElementById('amountCount').innerText = payBilAvailableBlanc;
+         })
+
+
+      // Transfer Money 
+          document.getElementById('transferBtn')
+      .addEventListener('click', function(e){
+         e.preventDefault()
+          const transferTotalAmount =parseInt(document.getElementById('transferAmount').value)
+          
+          const transferAllAmount = parseInt(document.getElementById('amountCount').innerText);
+          const transferAvailableBlanc = transferAllAmount - transferTotalAmount;
+          document.getElementById('amountCount').innerText = transferAvailableBlanc;
+         
+
+      })
+      
+
+//   Tggle
     document.getElementById('addMoneyTg')
       .addEventListener('click', function(){
           document.getElementById('cashOut-container').style.display ='none';
+          document.getElementById('transfer_container').style.display='none';
+          document.getElementById('bonus_container').style.display='none';
+
           document.getElementById('addMoney_container').style.display='block';
-         document.getElementById('addMoneyTg').style.backgroundColor = '#0874f20d';
-         document.getElementById('addMoneyTg').style.borderColor = '#0874f2';
-         
-          document.getElementById('cahOutTg').style.backgroundColor = '#ffffff';
-          document.getElementById('cahOutTg').style.borderColor = '#66BB6A';
+
+         // document.getElementById('addMoneyTg').style.backgroundColor = '#0874f20d';
+         // document.getElementById('addMoneyTg').style.borderColor = '#0874f2';
+
+         //  document.getElementById('cahOutTg').style.backgroundColor = '#ffffff';
+         //  document.getElementById('cahOutTg').style.borderColor = '#66BB6A';
       })
 
     document.getElementById('cahOutTg')
       .addEventListener('click', function(){
           document.getElementById('addMoney_container').style.display= 'none';
-          document.getElementById('cashOut-container').style.display='block';
-          document.getElementById('cahOutTg').style.backgroundColor = '#0874f20d';
-          document.getElementById('cahOutTg').style.borderColor = '#0874f2';
 
-          document.getElementById('addMoneyTg').style.backgroundColor = '#ffffff';
-         document.getElementById('addMoneyTg').style.borderColor = '#66BB6A';
+          document.getElementById('transfer_container').style.display='none';
+          document.getElementById('bonus_container').style.display='none';
+          
+          document.getElementById('cashOut-container').style.display='block';
+
+
+         //  document.getElementById('cahOutTg').style.backgroundColor = '#0874f20d';
+         //  document.getElementById('cahOutTg').style.borderColor = '#0874f2';
+
+         //  document.getElementById('addMoneyTg').style.backgroundColor = '#ffffff';
+         // document.getElementById('addMoneyTg').style.borderColor = '#66BB6A';
            
       })
-  
+
+      document.getElementById('transferMoneyTg')
+         .addEventListener('click', function(){
+         document.getElementById('addMoney_container').style.display='none';
+
+         document.getElementById('cashOut-container').style.display='none';
+         document.getElementById('bonus_container').style.display='none';
+         document.getElementById('transfer_container').style.display='block';
+            
+         })
+
+
+         // get Bonus 
+          
+         document.getElementById('getBonus')
+            .addEventListener('click', function(){
+         document.getElementById('addMoney_container').style.display='none';
+
+         document.getElementById('cashOut-container').style.display='none';
+         document.getElementById('transfer_container').style.display='none';
+         document.getElementById('bonus_container').style.display='block';
+
+
+            })
+
+
+            // pay BilTg
+
+            document.getElementById('payBillTG')
+               .addEventListener('click', function(){
+         document.getElementById('addMoney_container').style.display='none';
+         document.getElementById('cashOut-container').style.display='none';
+         document.getElementById('transfer_container').style.display='none';
+         document.getElementById('bonus_container').style.display='none';
+         document.getElementById('payBill_container').style.display='block';
+
+             })
+
+
+            //  transactionTg
+            document.getElementById('transactionTG')
+               .addEventListener('click', function(){
+         document.getElementById('addMoney_container').style.display='none';
+         document.getElementById('cashOut-container').style.display='none';
+         document.getElementById('transfer_container').style.display='none';
+         document.getElementById('bonus_container').style.display='none';
+         document.getElementById('payBill_container').style.display='none';
+         document.getElementById('transactions-container').style.display='block';
+               })
+
+
+
 
 
 
