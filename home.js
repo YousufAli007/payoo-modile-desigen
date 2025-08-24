29ee01dfdc780b4459b429c88921ec6e07ebdb51
yousuf ali <yousuf787700@gmail.com>
2025-08-24 20:48:38 +0600

    added some style

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 //  money add button
    const validPin = 6581;
-document.getElementById('moneyBtn')
+   document.getElementById('moneyBtn')
     .addEventListener('click', function(e){
          e.preventDefault()
          const account =document.getElementById('AccountNumber').value;
@@ -24,8 +24,39 @@ document.getElementById('moneyBtn')
     })
 
 
+
+  
+    document.getElementById('btn1')
+      .addEventListener('click', function(){
+          document.getElementById('cashOut_container').style.display ='none';
+          document.getElementById('addMoney_container').style.display='block'
+      })
+
+    document.getElementById('btn2')
+      .addEventListener('click', function(){
+          document.getElementById('addMoney_container').style.display ="none";
+          document.getElementById('cashOut_container').style.display='block';
+           
+      })
+  
+
+
+
+
+
+
+
+
     // add log Out btn
     document.getElementById('logOut')
         .addEventListener('click', function(){
            window.location.href="./index.html" 
         })
+
+
+
+
+
+
+
+
