012451f6a01f46aab0e5edf4cce1e5390d2c7e8c
yousuf ali <yousuf787700@gmail.com>
2025-08-24 10:08:39 +0600

    added home page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
- 
+//  Login button 
+
+document.getElementById('updateBtn')
+    .addEventListener('click', function(e){
+         e.preventDefault()
+         const number = 1842846581;
+         const pin = 6581;
+         const inputNumber =document.getElementById('input_Number');
+         const inputNumberValue =parseInt(inputNumber.value);
+
+        const inputPin =document.getElementById('input_pin');
+        const inputPinValue = parseInt(inputPin.value );
+         if(number === inputNumberValue && pin=== inputPinValue){
+           window.location.href="./home.html"
+         }
+         else{
+            alert('Invalid Credentials')
+         }
+    })
